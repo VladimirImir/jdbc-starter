8bb3ff840846ec350879932f14c379204aa985dd
VladimirImir <mimimvvv@gmail.com>
2023-06-28 16:44:32 +0300

    JDBC. 3. - Подключение к базам данных.

---
+++ src/com/dev/jdbc/starter/util/ConnectionManager.java
@@ -0,0 +1,34 @@
+package com.dev.jdbc.starter.util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public final class ConnectionManager {
+    private static final String URL = "jdbc:postgresql://localhost:5432/flight_repository";
+    private static final String USERNAME = "postgres";
+    private static final String PASSWORD = "postgres";
+
+    static {
+        loadDriver();
+    }
+
+    private ConnectionManager() {
+    }
+
+    private static void loadDriver() {
+        try {
+            Class.forName("org.postgresql.Driver");
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static Connection open() {
+        try {
+            return DriverManager.getConnection(URL, USERNAME, PASSWORD);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
