056b3e37ebacf272f6cec381a08e7dd8ce1b4ae8
VladimirImir <mimimvvv@gmail.com>
2023-06-28 17:07:18 +0300

    JDBC. 4. - Properties файл.

--- src/com/dev/jdbc/starter/util/ConnectionManager.java
+++ src/com/dev/jdbc/starter/util/ConnectionManager.java
@@ -5,9 +5,9 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public final class ConnectionManager {
-    private static final String URL = "jdbc:postgresql://localhost:5432/flight_repository";
-    private static final String USERNAME = "postgres";
-    private static final String PASSWORD = "postgres";
+    private static final String URL_KEY = "db.url";
+    private static final String USERNAME_KEY = "db.username";
+    private static final String PASSWORD_KEY = "db.password";
 
     static {
         loadDriver();
@@ -16,18 +16,22 @@ public final class ConnectionManager {
     private ConnectionManager() {
     }
 
-    private static void loadDriver() {
+    public static Connection open() {
         try {
-            Class.forName("org.postgresql.Driver");
-        } catch (ClassNotFoundException e) {
+            return DriverManager.getConnection(
+                    PropertiesUtil.get(URL_KEY),
+                    PropertiesUtil.get(USERNAME_KEY),
+                    PropertiesUtil.get(PASSWORD_KEY)
+            );
+        } catch (SQLException e) {
             throw new RuntimeException(e);
         }
     }
 
-    public static Connection open() {
+    private static void loadDriver() {
         try {
-            return DriverManager.getConnection(URL, USERNAME, PASSWORD);
-        } catch (SQLException e) {
+            Class.forName("org.postgresql.Driver");
+        } catch (ClassNotFoundException e) {
             throw new RuntimeException(e);
         }
     }
