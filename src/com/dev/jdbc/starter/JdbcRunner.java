2aaf0675fae81807aae99603c27ad5c9a9779a29
VladimirImir <mimimvvv@gmail.com>
2023-06-29 10:52:02 +0300

    JDBC. 6. - Statement. DML операции.

--- src/com/dev/jdbc/starter/JdbcRunner.java
+++ src/com/dev/jdbc/starter/JdbcRunner.java
@@ -13,17 +13,29 @@ public class JdbcRunner {
     public static void main(String[] args) throws SQLException {
         Class<Driver> driverClass = Driver.class;
         String sql = """
-                CREATE TABLE IF NOT EXISTS info (
+                /*CREATE TABLE IF NOT EXISTS info (
                     id SERIAL PRIMARY KEY ,
                     data TEXT NOT NULL 
-                );
+                );*/
+                /*INSERT INTO info (data)
+                VALUES 
+                ('Test1'),
+                ('Test2'),
+                ('Test3'),
+                ('Test4');*/
+                UPDATE info
+                SET data = 'TestTest'
+                WHERE id = 5
+                RETURNING *
                 """;
         try (var connection = ConnectionManager.open();
              var statement = connection.createStatement()) {
             System.out.println(connection.getSchema());
             System.out.println(connection.getTransactionIsolation());
             var executeResult = statement.execute(sql);
+            //var executeResult = statement.executeUpdate(sql);
             System.out.println(executeResult);
+            System.out.println(statement.getUpdateCount());
         }
     }
 }
