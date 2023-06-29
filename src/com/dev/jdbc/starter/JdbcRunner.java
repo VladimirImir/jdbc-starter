e85b636d0e84d0134bbb5ae9b703184d74268dca
VladimirImir <mimimvvv@gmail.com>
2023-06-29 10:40:25 +0300

    JDBC. 5. - Statement. DDL операции.

--- src/com/dev/jdbc/starter/JdbcRunner.java
+++ src/com/dev/jdbc/starter/JdbcRunner.java
@@ -6,14 +6,24 @@ import org.postgresql.Driver;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 public class JdbcRunner {
 
     public static void main(String[] args) throws SQLException {
         Class<Driver> driverClass = Driver.class;
-
-        try (var connection = ConnectionManager.open()) {
+        String sql = """
+                CREATE TABLE IF NOT EXISTS info (
+                    id SERIAL PRIMARY KEY ,
+                    data TEXT NOT NULL 
+                );
+                """;
+        try (var connection = ConnectionManager.open();
+             var statement = connection.createStatement()) {
+            System.out.println(connection.getSchema());
             System.out.println(connection.getTransactionIsolation());
+            var executeResult = statement.execute(sql);
+            System.out.println(executeResult);
         }
     }
 }
