c2b5d38d5811656982b66ab74dfd1045b95dcc3c
VladimirImir <mimimvvv@gmail.com>
2023-06-30 15:12:05 +0300

    JDBC. 18. - DAO. Entity mapping.

---
+++ src/com/dev/jdbc/starter/dao/TicketDao.java
@@ -0,0 +1,14 @@
+package com.dev.jdbc.starter.dao;
+
+public class TicketDao {
+
+    private static final TicketDao INSTANCE = new TicketDao();
+
+    private TicketDao() {
+
+    }
+
+    public static TicketDao getInstance() {
+        return INSTANCE;
+    }
+}
